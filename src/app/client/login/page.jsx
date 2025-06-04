46da97c25221f82feaec50f45553188ba4c769a4
yogu-code <yogeshrane019@gmail.com>
2025-06-04 12:55:16 +0530

    feat(auth): add LoginComponent for user authentication

---
+++ src/app/client/login/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ClientLoginComponenet from "@/components/ClientLoginCompoenent.jsx"
+export default function page() {
+  return (
+    <div>
+        <ClientLoginComponenet />
+    </div>
+  )
+}
